f03063db6df9275a073935a8c9728f8d6c8b588a
Mike Schutte <tmikeschutte@gmail.com>
2016-11-18 08:16:30 +0800

    updates get tasks route to read SQL

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,8 @@
 require 'sqlite3'
 
 class Task
+  attr_reader :title,
+              :description
 
   def initialize(task_params)
     @description              = task_params["description"]
@@ -11,6 +13,13 @@ class Task
 
   def save
     @database.execute("INSERT INTO tasks (title, description) VALUES (?, ?);", @title, @description)
+  end
+
+  def self.all
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
+    tasks = database.execute("SELECT * FROM tasks")
+    tasks.map { |task| Task.new(task) }
   end
 
 end
